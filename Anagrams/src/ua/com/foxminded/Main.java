ff198a759cc9bfdd732d5d68b09792509e1a4ebf
Rishat Suleymanov <spk_irbis@mail.ru>
2021-04-07 21:38:27 +0300

    Two tests completed successfully

--- Anagrams/src/ua/com/foxminded/Main.java
+++ Anagrams/src/ua/com/foxminded/Main.java
@@ -5,33 +5,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String...args) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			String input = reader.readLine();
-			System.out.print(revers(input));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String... args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String input = reader.readLine();
+            System.out.print(revers(input));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static String revers(String input) {
-		String result = "";
-		String[] sArrays = input.split(" ");
-		char tmp;
-		for (String string : sArrays) {
-			char[] charArrays = string.toCharArray();
-			for(int i = 0; i < string.length() / 2; i++) {
-				if (Character.isLetter(charArrays[i])) {
-					tmp = charArrays[i];
-					if (Character.isLetter(charArrays[string.length()-1-i])) {
-						charArrays[i] = charArrays[string.length()-1-i];
-						charArrays[string.length()-1-i] = tmp;
-					}
-				}
-			}
-			result = result + String.valueOf(charArrays) + " ";
-		}
-		return result.trim();
-	}
+    public static String revers(String input) {
+        String result = "";
+        String[] stringsArray = input.split(" ");
+        char tmp;
+
+        for (String string : stringsArray) {
+            char[] charsArray = string.toCharArray();
+            int indexOffsetToTheRight = 0;
+            int indexOffsetToTheLeft = 0;
+            for (int i = 0; i < string.length() / 2; i++) {
+                if (Character.isLetter(charsArray[i + indexOffsetToTheRight])) {
+                    tmp = charsArray[i + indexOffsetToTheRight];
+                    if (Character.isLetter(charsArray[string.length() - 1 - i - indexOffsetToTheLeft])) {
+                        charsArray[i] = charsArray[string.length() - 1 - i - indexOffsetToTheLeft];
+                        charsArray[string.length() - 1 - i - indexOffsetToTheLeft] = tmp;
+                    } else {
+                        indexOffsetToTheLeft++;
+                        charsArray[i + indexOffsetToTheRight] = charsArray[string.length() - 1 - i - indexOffsetToTheLeft];
+                        charsArray[string.length() - 1 - i - indexOffsetToTheLeft] = tmp;
+                    }
+                } else {
+                    indexOffsetToTheRight++;
+                    if (Character.isLetter(charsArray[i + indexOffsetToTheRight])) {
+                        tmp = charsArray[i + indexOffsetToTheRight];
+                        if (Character.isLetter(charsArray[string.length() - 1 - i - indexOffsetToTheLeft])) {
+                            charsArray[i + indexOffsetToTheRight] = charsArray[string.length() - 1 - i - indexOffsetToTheLeft];
+                            charsArray[string.length() - 1 - i - indexOffsetToTheLeft] = tmp;
+                        }
+                    }
+                }
+            }
+            result = result + String.valueOf(charsArray) + " ";
+        }
+        return result.trim();
+    }
 }
